61c7dcd948ebe41f9c595da4ee50fe356cc63855
OTDavid <tunjiobidokun@gmail.com>
2023-10-21 03:22:27 -0700

    Updates

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -2,10 +2,42 @@ const express = require("express")
 
 const router = express.Router()
 
-router.get("/", ()=>{
+const workout = require("../model/workoutModel")
 
-    res.send("Hellooo")
 
+//GET all workouts
+router.get("/", (req, res) => {
+    res.send("Get all workouts")
 })
+
+//GET a single workout
+router.get("/:id", (req, res) => {
+    res.send("Single Workout")
+})
+
+//POST a new workout
+router.post("/", async (req, res) => {
+    const { title, load, reps } = req.body
+    try {
+        const Workout = await Workout.create({ title, load, reps })
+        res.status(200).json({ workout })
+
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+
+    }
+})
+
+//DELETE a single workout
+router.delete("/:id", (req, res) => {
+    res.send("Delete a Workout")
+})
+
+//UPDATE a single workout
+router.patch("/:id", (req, res) => {
+    res.send("UpdateSingle Workout")
+})
+
+
 
 module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,34 @@
 
 const express = require("express")
-const app = express()
+const mongoose = require("mongoose")
 
 require('dotenv').config()
 const WorkoutsRoutes = require("./routes/workouts")
+const e = require("express")
 
 const PORT = process.env.PORT
+const database = process.env.MONGO_URI
+
+//Express App
+const app = express()
+
+//Middleware
+app.use(express.json())
 
 
-app.get("/", (req, res)=>{
-    // res.json({"hello World": "a"})
-    res.send("Hello World")
-})
+//Routes
 
+app.use("/api/workouts", WorkoutsRoutes)
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+//Connect to db
+mongoose.connect(database)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Connected to database and running on port ${PORT}`);
+            
+        })
+
+    }).catch((error) => {
+        console.log(error)
+    })
+//Listening at port
