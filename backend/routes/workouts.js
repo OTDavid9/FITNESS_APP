9662b16bb747ae55880af74a4c8f64ae033bc1b8
OTDavid <tunjiobidokun@gmail.com>
2023-10-21 03:23:24 -0700

    Updates

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,42 +1,31 @@
 const express = require("express")
+const { createWorkout,
+    getWorkouts,
+    getWorkout,
+    deleteWorkout,
+    updateWorkout,
+
+} = require('../controller/workoukController')
 
 const router = express.Router()
 
-const workout = require("../model/workoutModel")
+const Workout = require("../model/workoutModel")
 
 
 //GET all workouts
-router.get("/", (req, res) => {
-    res.send("Get all workouts")
-})
+router.get("/", getWorkouts)
 
 //GET a single workout
-router.get("/:id", (req, res) => {
-    res.send("Single Workout")
-})
+router.get("/:id", getWorkout)
 
 //POST a new workout
-router.post("/", async (req, res) => {
-    const { title, load, reps } = req.body
-    try {
-        const Workout = await Workout.create({ title, load, reps })
-        res.status(200).json({ workout })
-
-    } catch (error) {
-        res.status(400).json({ error: error.message })
-
-    }
-})
+router.post("/", createWorkout)
 
 //DELETE a single workout
-router.delete("/:id", (req, res) => {
-    res.send("Delete a Workout")
-})
+router.delete("/:id", deleteWorkout)
 
 //UPDATE a single workout
-router.patch("/:id", (req, res) => {
-    res.send("UpdateSingle Workout")
-})
+router.patch("/:id", updateWorkout)
 
 
 
